a21793a4c5429bd177536547d8e5a1e3f01d1f85
Eduardo Diniz <eduardo.diniz@yandeh.com.br>
2021-06-24 21:36:21 -0300

    Adicionando mecanismo de interação com as perguntas
    -> Adição de destaque, exclusão e resposta de perguntas (admin)
    -> Adição do README.md

---
+++ README.md
@@ -0,0 +1,33 @@
+# Let Me Ask
+Aplicativo de Perguntas e Respostas criado durante a Next Level Week (NLW) organizado em Junho de 2021 - Trilha de ReactJS
+
+## Tecnologias utilizadas
+&rarr; ReactJS
+&rarr; TypeScript
+&rarr; Firebase
+&rarr; Sass
+
+## Principais funcionalidades
+- Autenticação com conta do Google
+- Recurso de criação de salas de Perguntas e Respostas
+- Entrar em salas existentes com código de uma sala
+- Criar e dar gostei em perguntas
+- Excluir, responder e destacar perguntas (admin)
+- Encerrar salas
+
+## Executar o projeto local
+Yarn
+```sh
+yarn install
+yarn start
+```
+
+## Variáveis de ambiente
+```properties
+REACT_APP_FIREBASE_API_KEY="var_firebase_api_key"
+REACT_APP_FIREBASE_AUTH_DOMAIN="var_firebase_auth_domain"
+REACT_APP_FIREBASE_PROJECT_ID="var_firebase_project_id"
+REACT_APP_FIREBASE_STORAGE_BUCKET="var_firebase_storage_bucket"
+REACT_APP_FIREBASE_MESSAGING_SENDER_ID="var_firebase_messaging_sender_id"
+REACT_APP_FIREBASE_APP_ID="var_firebase_app_id"
+```
--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,6 +1,8 @@
 import { useHistory, useParams } from 'react-router-dom'
 import logoImg from '../assets/images/logo.svg'
 import deleteImg from '../assets/images/delete.svg'
+import checkImg from '../assets/images/check.svg'
+import answerImg from '../assets/images/answer.svg'
 import { Button } from '../components/Button'
 import { Question } from '../components/Question'
 import { RoomCode } from '../components/RoomCode'
@@ -23,6 +25,18 @@ export function AdminRoom() {
     if (window.confirm('Tem certeza que você deseja excluir esta pergunta?')) {
       await database.ref(`rooms/${roomId}/questions/${questionId}`).remove()
     }
+  }
+
+  async function handleCheckQuestionAsAnswered(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: true
+    })
+  }
+
+  async function handleHighlightQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHighlighted: true
+    })
   }
 
   async function handleEndRoom() {
@@ -53,7 +67,7 @@ export function AdminRoom() {
       <main>
         <div className="room-title">
           <h1>Sala {title}</h1>
-          {questions.length && <span>{questions.length} pergunta(s)</span>}
+          {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
         </div>
 
         <div className="question-list">
@@ -62,7 +76,24 @@ export function AdminRoom() {
               key={question.id}
               content={question.content}
               author={question.author}
+              isAnswered={question.isAnswered}
+              isHighlighted={question.isHighlighted}
             >
+              {!question.isAnswered && (
+                <>
+                  <button
+                    type="button"
+                    onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                  >
+                    <img src={checkImg} alt="Responder pergunta" />
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => handleHighlightQuestion(question.id)}
+                  >
+                    <img src={answerImg} alt="Destaque pergunta" />
+                  </button></>
+              )}
               <button
                 type="button"
                 onClick={() => handleDeleteQuestion(question.id)}
